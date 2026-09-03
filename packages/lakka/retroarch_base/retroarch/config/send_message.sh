8be2237c54dd1ec792fa180f09af6a7c24aba8a0
dumpstertree <dumpstertree@gmail.com>
2026-09-03 05:54:43 -0400

    Create send_message.sh

---
+++ packages/lakka/retroarch_base/retroarch/config/send_message.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+HID="/dev/hidraw0"
+
+CMD_READY=0x01
+CMD_NO_GAME=0x02
+CMD_NO_CORE=0x03
+CMD_GAME_START=0x04
+CMD_GAME_EXIT=0x05
+
+send() {
+    printf '\x02\x%02x' "$1" > "$HID"
+}
+
+case "$1" in
+    ready)
+        send "$CMD_READY"
+        ;;
+    no-game)
+        send "$CMD_NO_GAME"
+        ;;
+    no-core)
+        send "$CMD_NO_CORE"
+        ;;
+    game-start)
+        send "$CMD_GAME_START"
+        ;;
+    game-exit)
+        send "$CMD_GAME_EXIT"
+        ;;
+esac
