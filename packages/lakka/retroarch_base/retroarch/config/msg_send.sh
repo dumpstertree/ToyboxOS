97921cf6e1523433520066be51ae95460636389e
dumpstertree <dumpstertree@gmail.com>
2026-09-03 14:22:38 -0400

    Update msg_send.sh

--- packages/lakka/retroarch_base/retroarch/config/msg_send.sh
+++ packages/lakka/retroarch_base/retroarch/config/msg_send.sh
@@ -6,7 +6,6 @@ CMD_READY=0x01
 CMD_NO_GAME=0x02
 CMD_NO_CORE=0x03
 CMD_GAME_START=0x04
-CMD_GAME_EXIT=0x05
 
 send() {
     printf '%b' "$(printf '\\x02\\x%02x' "$1")" > "$HID"
@@ -14,22 +13,23 @@ send() {
 
 case "$1" in
     ready)
+        system-log "Command sent: READY"
         send "$CMD_READY"
         ;;
     no-game)
+        system-log "Command sent: NO GAME"
         send "$CMD_NO_GAME"
         ;;
     no-core)
+        system-log "Command sent: NO CORE"
         send "$CMD_NO_CORE"
         ;;
     game-start)
+        system-log "Command sent: GAME START"
         send "$CMD_GAME_START"
         ;;
-    game-exit)
-        send "$CMD_GAME_EXIT"
-        ;;
     *)
-        echo "Usage: $0 {ready|no-game|no-core|game-start|game-exit}"
+        echo "Usage: $0 {ready|no-game|no-core|game-start}"
         exit 1
         ;;
 esac
